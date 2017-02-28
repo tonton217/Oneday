d9b62e3c7b4eb7c4dec78449a03b519026db9237
wjjung <wjjung@example.com>
2017-02-28 17:33:59 +0900

    Added: Add libraries (Realm, Lombok)

---
+++ app/src/androidTest/java/tonton217/io/oneday/realm/Person.java
@@ -0,0 +1,22 @@
+package tonton217.io.oneday.realm;
+
+/**
+ * @author wjjung
+ */
+class Person {
+    private final String name;
+    private final int age;
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
---
+++ app/src/androidTest/java/tonton217/io/oneday/realm/RealmTests.java
@@ -0,0 +1,9 @@
+package tonton217.io.oneday.realm;
+
+/**
+ * @author wjjung
+ * @since 2017-02-28.
+ */
+
+public class RealmTests {
+}
